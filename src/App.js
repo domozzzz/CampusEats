759430a681aaf024df5f7d14672524ac1cb61f5d
AP <882domokun882@gmail.com>
2024-08-16 08:37:39 +1000

    added all pages in nav header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 import Home from './pages/Home.js';
+import About from './pages/About.js'
 import Meals from './pages/Meals.js';
+import Recipes from './pages/Recipes.js';
+import Contact from './pages/Contact.js'
 import Login from './pages/Login.js';
 import Header from './components/Header.js';
 import Footer from './components/Footer.js';
 import './App.css';
+
 
 function App() {
   return (
@@ -14,7 +18,10 @@ function App() {
 
       <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
           <Route path="/meals" element={<Meals />} />
+          <Route path="/recipes" element={<Recipes />} />
+          <Route path="/contact" element={<Contact />} />
           <Route path="/login" element={<Login />} />
       </Routes>
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -7,17 +7,31 @@ export default function Header() {
     <nav>
       <ul>
 
-      <li><Link to = "/">
-            <a href="/">Home</a>
-      </Link></li>
+        <li><Link to = "/">
+              <a href="/">Home</a>
+        </Link></li>
 
-      <li><Link to = "/meals">
+        <li><Link to = "/about">
+              <a href="about">About</a>
+        </Link></li>
+
+        <li><Link to = "/meals">
           <a href="/meals">Meals</a>
         </Link></li>
+
+        <li><Link to = "/recipes">
+          <a href="/recipes">Recipes</a>
+        </Link></li>
+
+        <li><Link to = "/contact">
+          <a href="/contact">Contact</a>
+        </Link></li>
+
         <li><Link to = "/login">
           <a href="/login">Login</a>
         </Link></li>
-        </ul>
-      </nav>
+
+      </ul>
+    </nav>
   );
 }
