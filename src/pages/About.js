5a51fc87a2ce1e81e866bea9d9a49406f9bfd85c
Mrdhtillah <mardha.ananti@gmail.com>
2024-08-19 18:35:27 +1000

    Update About page: Adjust styles and add new image
    - Updated styles in App.css to enhance About page appearance.
    - Added about.jpeg to the images folder.
    - Modified About.js to include the new image and adjust layout.

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,22 @@
 
-export default function Home() {
+import about from '../images/about.jpeg'
+
+export default function About() {
     return (
-        <p>Home</p>
+        <div>
+            <div class="about" alt="Avatar" style={{ width: "100%", height: "600px" }}>
+                <div className="about-image">
+                    <img src={about} alt="Avatar" style={{ width: "100%", height: "100%" }}></img>
+                </div>
+                <div className="about-text" style={{}}>
+                    <h1>About Us</h1>
+                    <p>
+                        At CampusEats, our mission is to make healthy eating accessible and convenient <br></br>for university students. We believe that nutritious meals
+                        are essential for academic success and overall well-being.<br></br> Our goal is to deliver fresh, wholesome meal kits that save time and provide 
+                        the energy needed to excel in both studies and life.
+                    </p>
+                </div>
+            </div>
+        </div>
     );
 }
