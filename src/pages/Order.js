8d5eb994590cb801ff2a63ef1a740bdc877a5786
AP <882domokun882@gmail.com>
2024-09-12 17:57:10 +1000

    order page with popup and filter button

--- src/pages/Order.js
+++ src/pages/Order.js
@@ -5,46 +5,56 @@ import chickenRice from '../images/Chicken rice.png'
 import kebab from '../images/Kebab.png'
 import React, { useState } from 'react';
 
-function PopUpMenu() {
-    return (
-      <ul className="drop-down">
-        <li>Menu-item-1</li>
-        <li>Menu-item-2</li>
-        <li>Menu-item-3</li>
-      </ul>
-    );
-  }
+function PopUp() {
+  const [seen, setSeen] = useState(false)
+
+  function togglePop () {
+      setSeen(!seen);
+  };
+
+  return (
+      <div>
+          <button onClick={togglePop}>Login</button>
+          {seen ? <button /> : null}
+      </div>
+  )
+}
+
+
 
 const Dropdown = () => {
-  // State to manage the dropdown visibility and selected value
   const [isOpen, setIsOpen] = useState(false);
   const [selectedValue, setSelectedValue] = useState('Popular');
 
-  // Toggle dropdown visibility
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
   };
 
-  // Handle selecting a dropdown option
+  const closeDropdown = () => {
+    setIsOpen(false);
+  };
+
   const handleButtonClick = (value) => {
     setSelectedValue(value);
-    setIsOpen(false); // Close the dropdown after selecting an option
+    setIsOpen(false);
   };
 
   return (
-    <div className="dropdown">
+    <div className="dropdown" onMouseLeave={closeDropdown}
+>
       <button 
         className="dropdown-button" 
-        onMouseOver={toggleDropdown}
+        onClick={toggleDropdown}
       >
-        {selectedValue} <i className={`arrow ${isOpen ? 'up' : 'down'}`}></i>
+        {selectedValue}
       </button>
 
       {isOpen && (
         <div className="dropdown-content">
-
+          <button
+            className="dropdown-button-open" 
+          >{selectedValue}</button>
           <button 
-            to="/order" 
             className="dropdown-option-button" 
             onClick={() => handleButtonClick('Popular')}
           >
@@ -52,7 +62,6 @@ const Dropdown = () => {
           </button>
 
           <button 
-            to="/order" 
             className="dropdown-option-button" 
             onClick={() => handleButtonClick('Price')}
           >
@@ -72,6 +81,75 @@ const Dropdown = () => {
 };
 
 export default function About() {
+
+  const [counter, setCounter] = useState(0);
+
+  const inc = () => {
+    setCounter(counter + 1 );
+  }
+
+  const [seen, setSeen] = useState(false)
+
+  function openPop () {
+    setSeen(true);
+  };
+
+  function closePop () {
+    setSeen(false);
+  };
+
+  function addToCart() {
+    inc();
+    closePop();
+  }
+
+
+    const Pop = () => {
+      return (
+        <div className='pop'>
+          <button className="exit-button" onClick={closePop}>x</button>
+          <h2>Braised Brisket Burrito</h2>
+          <img src={burrito} alt="Avatar"></img>
+
+          <table>
+            <tr>
+              <th>Nutritient</th>
+              <th>Amount per Serving</th>
+            </tr>
+            <tr>
+              <td>Calories</td>
+              <td>600 grams</td>
+            </tr>
+            <tr>
+              <td>Fats</td>
+              <td>400 grams</td>
+            </tr>
+          </table>
+
+          <div className='pop-info'>
+            <h4>Ingredients</h4>
+            <p>
+
+              Braised Beef:<br/>
+              Lean beef chuck (6 oz)<br/>
+              Beef broth (low-sodium)<br/>
+              Onions, garlic, and spices (cumin, paprika, etc.)<br/>
+              Burrito Components:<br/>
+              Whole-grain tortilla (large)<br/>
+              Black beans (1/2 cup)<br/>
+              Brown rice (1/2 cup)<br/>
+              Fresh vegetables (lettuce, tomatoes, bell peppers)<br/>
+              Cheese (optional, 1/4 cup)<br/>
+              Salsa or pico de gallo (2 tbsp)<br/>
+              Avocado or guacamole (optional, 1/4 avocado)</p>
+          </div>
+
+          <h4>$14 - 1 serving</h4>
+          <button onClick={addToCart} className='pop-button'>Add to Cart</button>
+        </div>
+      )
+    }
+
     return (
         <div>
             <div class="welcome" alt="Avatar">
@@ -83,48 +161,40 @@ export default function About() {
                 </div>
             </div>
             <div class="order">
-                <h1>Order One Meal</h1>
-                <div class="strip">
-                    <Link to="/cart" className="cart-button">Cart: 0</Link>
+            <div>
+                    <Link to="/cart" className="cart-button">Cart: {counter}</Link>
                     <Dropdown></Dropdown>
-                </div>
-                <div class="cards">
-                    <Link to="/meals">
-                        <div class="card">
+                <h1>Order One Meal</h1>
+
+              </div>
+              <div>
+            {seen ? <Pop/> : null}
+        </div>
+          <div className={`cards ${seen ? 'transparent' : ''}`}>
+                  <div class="card" onClick={openPop}>
                             <img src={burrito} alt="Avatar"></img>
                             <p>Braised Brisket Burrito</p>
                         </div>
-                    </Link>
-                    <Link to="/meals">
-                        <div class="card">
+                        <div class="card" onclick="openForm()">
                             <img src={chickenRice} alt="Avatar"></img>
                             <p>Hainanese Chicken Rice</p>
                         </div>
-                    </Link>
-                    <Link to="/meals">
                         <div class="card">
                             <img src={kebab} alt="Avatar"></img>
                             <p>Adana Kebab</p>
                         </div>
-                    </Link>
-                    <Link to="/meals">
                         <div class="card">
                             <img src={burrito} alt="Avatar"></img>
                             <p>Braised Brisket Burrito</p>
                         </div>
-                    </Link>
-                    <Link to="/meals">
-                        <div class="card">
+                        <div class="card" onclick="openForm()">
                             <img src={chickenRice} alt="Avatar"></img>
                             <p>Hainanese Chicken Rice</p>
                         </div>
-                    </Link>
-                    <Link to="/meals">
                         <div class="card">
                             <img src={kebab} alt="Avatar"></img>
                             <p>Adana Kebab</p>
                         </div>
-                    </Link>
                 </div>
             </div>
         </div>
