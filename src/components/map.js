f08dccef53ceb35cde19a01925e292112da19896
Jack Hume <jack64rh@gmail.com>
2024-10-01 15:19:56 +1000

    Added working map function

--- src/components/map.js
+++ src/components/map.js
@@ -1,39 +1,52 @@
 
 
-import React, {useEffect, useState, useRef, useCallback} from 'react';
+import React, {useEffect, useState, useRef, useCallback,useMemo} from 'react';
 import homepage from '../images/homepage.png'
+import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
+import "../css/Map.css"
+import "../css/App.css"
+import supabase from '../supabase';
 
-import {
-  APIProvider,
-  Map,
-  useMap,
-  AdvancedMarker,
-  MapCameraChangedEvent,
-  Pin
-} from '@vis.gl/react-google-maps';
+const MAP_API_KEY = '***PUT API KEY HERE***'
 
-const MAP_API_KEY = 'Not yet set'
-
-const locations = [
-  {key: 'operaHouse', location: { lat: -33.8567844, lng: 151.213108  }},
-  {key: 'tarongaZoo', location: { lat: -33.8472767, lng: 151.2188164 }},
-  {key: 'manlyBeach', location: { lat: -33.8209738, lng: 151.2563253 }},
-  {key: 'hyderPark',  location: { lat: -33.8690081, lng: 151.2052393 }},
-  {key: 'theRocks',   location: { lat: -33.8587568, lng: 151.2058246 }},
-  {key: 'circularQuay', location: { lat: -33.858761, lng: 151.2055688 }},
-  {key: 'harbourBridge', location: { lat: -33.852228, lng: 151.2038374 }},
-  {key: 'kingsCross', location: { lat: -33.8737375, lng: 151.222569 }},
-  {key: 'botanicGardens', location: { lat: -33.864167, lng: 151.216387 }},
-  {key: 'museumOfSydney', location: { lat: -33.8636005, lng: 151.2092542 }},
-  {key: 'maritimeMuseum', location: { lat: -33.869395, lng: 151.198648 }},
-  {key: 'kingStreetWharf', location: { lat: -33.8665445, lng: 151.1989808 }},
-  {key: 'aquarium', location: { lat: -33.869627, lng: 151.202146 }},
-  {key: 'darlingHarbour', location: { lat: -33.87488, lng: 151.1987113 }},
-  {key: 'barangaroo', location: { lat: - 33.8605523, lng: 151.1972205 }},
-];
+const UQ = {lat: -27.4977, lng: 153.0129}
 
 const MapDisplay = () => {
-      let pos = {lat: -27.496082133387972, lng: 153.01187003175372}
+
+      const { isLoaded } = useLoadScript({
+        googleMapsApiKey: MAP_API_KEY,
+      });
+      const [locations, setLocations] = useState([])
+      const [search, setSearch] = useState([])
+      const [display, setDisplay] = useState(null)
+
+      useEffect(() => {
+        async function getLocations() {
+          const {data, error} = await supabase
+          .from('Locations')
+          .select('*')
+
+          if (data) {
+            setLocations(data)
+          }
+
+          if (error) {
+            console.log(error)
+          }
+        }
+        getLocations()
+      },[])
+      
+      const get_search = (e) => {
+        setSearch(locations.filter((location) => location.name
+        .toLowerCase()
+        .includes(e.target.value.toLowerCase())))
+      }
+
+      const set_display = (e) => {
+        const displayLocation = locations.find((location) => location.id == e.target.name)
+        setDisplay(displayLocation)
+      }
       
       return (
         <div>
@@ -42,14 +55,31 @@ const MapDisplay = () => {
                   <img src={homepage} alt="Avatar" style={{zIndex: "0", width: "100%", height: "100vh", position: "relative"}}></img>
               </div>
           </div>
-          <APIProvider apiKey={MAP_API_KEY}>
-            <div style={{height: "600px"}}>
-              <Map zoom={11} center={pos} mapId={'50b77c7700aaebef'}/>
-              <AdvancedMarker position={{lat: pos.lat, lng: pos.lng}} onClick={() => {console.log("hello")}}>
-                <Pin background={"blue"}/>
-              </AdvancedMarker>
+          <div className='Map'>
+            <h1>Select your location</h1>
+            <div className='search-box'>
+                  <input type="search"  placeholder="Search for a location..." onChange={get_search} />
+                  {search.map((location) => {
+                    {console.log(location.id)}
+                    return (
+                    <div key={location.id} className='possible_location'> 
+                        <button className={display ? 'inactive' : ''} name={location.id} onClick={set_display}>{location.name}</button>
+                    </div>)
+                  })}
             </div>
-          </APIProvider>
+          {!isLoaded || !display? (
+            <h1></h1>
+          ) : (
+            <div>
+            <GoogleMap
+              mapContainerClassName='map-style'
+              center={{lat: display.lat, lng: display.lng}}
+              zoom={16}>
+            </GoogleMap>
+            <button className='submit_location'>Select {display.name}</button>
+            </div>
+          )}
+        </div>          
       </div>
       )
 }
