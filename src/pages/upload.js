4e202ff74f41011fd73cf781ce1faab4aa3aab79
Jack Hume <jack64rh@gmail.com>
2024-10-10 18:24:30 +1000

    Styled Upload with bootstrap

--- src/pages/upload.js
+++ src/pages/upload.js
@@ -1,13 +1,20 @@
 import React, {useState} from "react";
 import homepage from '../images/Homepage.png'
 import {Helmet} from 'react-helmet-async'
-import "../css/Upload.css"
-import "../css/App.css"
-import UploadPopup from "../components/UploadPopUp";
+import styles from "../css/Upload.module.css"
 import { useAuth } from '../components/AuthProvider';
 import supabase from "../supabase";
 import { Navigate } from "react-router-dom";
 import {v4 as uuidv4} from 'uuid';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Form from 'react-bootstrap/Form'
+import Button from "react-bootstrap/Button";
+import ListGroup from "react-bootstrap/ListGroup";
+import Stack from 'react-bootstrap/Stack'
+import Alert from 'react-bootstrap/Alert'
 
 //Empty ingredients object for user when inputting ingredients
 const baseIngreident = {
@@ -149,7 +156,8 @@ export default function Upload() {
         <div className="Basic_info">
             <h2>Basic Details</h2>
             <div className="basic_input">
-                <input type="text" placeholder="Meal Kit Name" onChange={e => setUpload({...upload,['name']: e.target.value})}/>
+                <Form.Label>Meal Kit Name</Form.Label>
+                <Form.Control type="text" onChange={e => setUpload({...upload,['name']: e.target.value})} />
                 <input type="text" placeholder="Image Link" onChange={e => setUpload({...upload,['image']: e.target.value})}/>
             </div>
         </div>
@@ -232,50 +240,109 @@ export default function Upload() {
  
     return user ? (
         <div>
-            <Helmet>
-                <title>Upload page</title>
-                <script src="https://kit.fontawesome.com/772a7aab14.js" crossorigin="anonymous"></script>
-            </Helmet>
             <div className="welcome" alt="Avatar">
                 <div className="heading-image">
                     <img src={homepage} alt="Avatar" style={{zIndex: "0", width: "100%", height: "100vh", position: "relative"}}></img>
                 </div>
             </div>  
-            <div className="upload">
-                <div className="header">
-                <h1>Upload</h1>
-                <h3>Upload your own meal-kits for review from our CampusEats team. Accepted Meal-Kits will be displayed on our market place and avaliable for purchase.</h3>
-                </div>
-                <div className="create-upload">
-                    <div className="detail-name">
-                        <ul>
-                            <li style={display==0 ? {'color': '#064d42', 'fontSize': '2rem'} : {'color':'#D9E5E3', 'fontSize': '1.5rem'}}>Basic Details</li>
-                            <li style={display==1 ? {'color': '#064d42', 'fontSize': '2rem'} : {'color':'#D9E5E3', 'fontSize': '1.5rem'}}>Dietary Info</li>
-                            <li style={display==2 ? {'color': '#064d42', 'fontSize': '2rem'} : {'color':'#D9E5E3', 'fontSize': '1.5rem'}}>Ingredients</li>
-                        </ul>
-
-                    </div>
-                    <div className="details">
-                        {options[display]}              
-                    </div>
-                    <div className="arrows">
-                        <i class="fa-solid fa-arrow-up" onClick={arrow_up}></i>
-                        <i class="fa-solid fa-arrow-down" onClick={arrow_down}></i>
-                    </div>
-                    <div className="arrows small">
-                    <i class="fa-solid fa-arrow-left" onClick={arrow_up}></i>    
-                    <i class="fa-solid fa-arrow-right" onClick={arrow_down}></i>
-                    </div>
-                </div>
-                <div className="submit_upload">
-                    <input type="file" accept="image/*" onChange={(e) => addImage(e)}/>
-                    <button onClick={handleSubmit}>Submit Meal-kit</button>
-                    <p style={{color: 'red'}}>{errorSubmit ? errorSubmit : ''}</p>
-                    <p style={{color: 'green'}}>{successful ? successful : ''}</p>
-                    <p>*All Meal-kits will be subjected to a review process by our CampusEats team.
-                         Ensure all information you provide is factual and accurate. Only select meal kits will be displayed within
-                         the marketplace and avaliable for order</p>
-                </div>
+            <div className={styles["upload"]}>
+                    <Container fluid>
+                        <Row lg="8" md="8" sm="10">
+                        <div className={styles["header"]}>
+                        <h1>Upload</h1>
+                        <p>Upload your own meal-kits for review from our CampusEats team. Accepted Meal-Kits will be displayed on our market place and avaliable for purchase.</p>
+                        </div>
+                        </Row>
+                        <Row className={`${styles['section']} mx-auto col-md-8`}>
+                                    <h2>Basic Details</h2>
+                                    <Form.Label>Meal Kit Name</Form.Label>
+                                    <Form.Control type="text" onChange={e => setUpload({...upload,['name']: e.target.value})} />
+                                    <Form.Label>Choose an Image</Form.Label>
+                                    <Form.Control type="file" accept="image/*" onChange={(e) => addImage(e)} />    
+                        </Row>
+                        <Row className={`${styles['section']} mx-auto col-md-8`}>
+                                <h2>Dietary Info</h2>
+                                <p>Check the boxes for relevant dietary info which applies to your meal</p>
+                                    <Form style={{'flex-direction': 'row'}}>
+                                        <Form.Check 
+                                            inline
+                                            className={styles['option']}
+                                            label="Vegetarian" 
+                                            type="checkbox" 
+                                            onChange={e => setUpload({...upload,['vegetarian']: e.target.value === 'on' ? true : false})}
+                                    />
+                                        <Form.Check
+                                            inline
+                                            label="Gluten-Free" 
+                                            className={styles['option']}
+                                            type="checkbox" 
+                                            onChange={e => setUpload({...upload,['gf']: e.target.value ==='on' ? true : false})}
+                                        />
+                                        <Form.Check 
+                                            inline
+                                            label="Vegan" 
+                                            className={styles['option']}
+                                            type="checkbox" 
+                                            onChange={e => setUpload({...upload,['vegan']: e.target.value === 'on' ? true : false})}
+                                        />    
+                                </Form>
+                        </Row>
+                        <Row className={`${styles['section']} mx-auto col-md-8`}>
+                                <h2>Ingredients</h2>
+                                <ListGroup  className={styles['ingredients']}>
+                                    {ingredients.map((item, i) => {
+                                        return (
+                                                <ListGroup.Item variant="success"> {item.name} x {item.quantity} {item.measurement} <Button 
+                                                className="ml-auto" variant="success" name={`Button-${i}`} onClick={removeIngredient}>-</Button>
+                                                </ListGroup.Item>
+                                        )
+                                    })}                                                              
+                                </ListGroup>
+                                    <Form>
+                                        <Row>
+                                            <Col xs="4">
+                                        <Form.Control value={newIngredient['name'] != null ? newIngredient['name'] : ''} placeholder="ingredient name" onChange={ e => setNewIngredient({...newIngredient, ['name']: e.target.value})}/>
+                                            </Col>
+                                            <Col xs="1">
+                                                <Form.Label>x</Form.Label>
+                                            </Col>
+                                            <Col xs="2">
+                                                <Form.Control type="number" min ="1" placeholder="1" onChange={ e => setNewIngredient({...newIngredient, ['quantity']: e.target.value})}/>
+                                            </Col>
+                                            <Col xs="3">
+                                                <Form.Select onChange={ e => setNewIngredient({...newIngredient, ['measurement']: e.target.value})}>
+                                                <option value="grams">grams</option>
+                                                <option value="milli-grams">milli-grams</option>
+                                                <option value = "litres">litres</option>
+                                                <option value = "milli-litres">milli-litres</option>
+                                                <option value="tablespoon">tablespoon</option>
+                                                <option value="item">item</option>
+                                                </Form.Select>
+                                            </Col>
+                                            <Col xs="2">
+                                                <Button variant="secondary" onClick={addIngredient}>Add</Button>
+                                            </Col>
+                        
+                                        </Row>
+                                    </Form>    
+                                    <div>
+                                        <p style={{color:'red'}}>{errorIngredient ? errorIngredient : ''}</p>
+                                    </div>
+                        </Row>
+                        <Stack className={`mx-auto col-lg-4 ${styles['section']}`} gap={2}>
+                            <Button size="lg" onClick={handleSubmit} variant="success">Submit Meal-kit</Button>
+                            {errorSubmit ? 
+                            <Alert variant="danger" dismissible>
+                                <Alert.Heading>Oh, No!</Alert.Heading>
+                                <p>{errorSubmit}</p>
+                            </Alert> : ''}
+                            {successful ? 
+                            <Alert variant="success" dismissible>
+                                <Alert.Heading>Uploaded!</Alert.Heading>
+                                <p>{successful}</p>
+                            </Alert> : ''}
+                        </Stack>
+                    </Container>
             </div>  
         </div>
     ) : <Navigate to='/login'/>
